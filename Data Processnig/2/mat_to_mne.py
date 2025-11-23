93e9f4cba9e38f7382a8d7d461bea4006b6ed87c
AleczO <alecz0.code@gmail.com>
2025-11-23 23:27:03 +0100

    Update

---
+++ Data Processnig/2/mat_to_mne.py
@@ -0,0 +1,50 @@
+import scipy.io
+import mne
+import matplotlib.pyplot as plt
+
+def process_for_psd():
+    mat_raw = scipy.io.loadmat("Data/P01_Ses1.mat")
+    raw_array = mat_raw["data_ses1"][1:]
+
+    ch_names = ["PO3", "POz", "PO4", "O1", "Oz", "O2", "02", "03", "04"]
+    ch_types = ["eeg"] * 6 + ['misc'] * 2 + ['stim']
+
+    info = mne.create_info(ch_names = ch_names, ch_types = ch_types, sfreq=512)
+    info.set_montage("standard_1020")
+
+    raw = mne.io.RawArray(raw_array, info)
+    raw.pick_types(eeg=True)
+
+    return raw 
+
+
+def process():
+    mat_raw = scipy.io.loadmat("Data/P01_Ses1.mat")
+    raw_array = mat_raw["data_ses1"][1:]
+
+    ch_names = ["PO3", "POz", "PO4", "O1", "Oz", "O2", "02", "03", "04"]
+
+    info = mne.create_info(ch_names = ch_names, sfreq=512)
+    info.set_montage("standard_1020")
+
+    raw = mne.io.RawArray(raw_array, info)
+
+    return raw 
+
+
+
+def Hz_channels():
+    mat_raw = scipy.io.loadmat("Data/P01_Ses1.mat")
+
+    plt.plot(mat_raw["data_ses1"][0], mat_raw["data_ses1"][8])
+    plt.plot(mat_raw["data_ses1"][0], mat_raw["data_ses1"][9] * 1e-6)
+    plt.show()
+
+
+
+Hz_channels()
+
+raw = process_for_psd()
+
+raw = process()
+raw.plot(block=True, scalings='auto')
--- README.md
+++ README.md
@@ -2,18 +2,13 @@
 
 This repository provides notes, methodologies, and reference materials for working with Steady-State Visually Evoked Responses (SSVEP), including analysis techniques and experimental considerations.
 
-<br><br>
-
 # Used Datasets
 
-- [MAMEM Project Datasets](https://www.mamem.eu/results/datasets/)
+1. [MAMEM Project Datasets](https://www.mamem.eu/results/datasets/)
 
-- [8-Channel SSVEP EEG Dataset with Artifacts Trials](https://openneuro.org/datasets/ds004745/versions/1.0.0)
+2. [8-Channel SSVEP EEG Dataset with Artifacts Trials](https://openneuro.org/datasets/ds004745/versions/1.0.0)
 
-
-<br><br>
-
-
+3. [Multi-frequency steady-state visual evoked potential dataset](https://www.nature.com/articles/s41597-023-02841-5?fromPaywallRec=false#Sec17) ([access](https://figshare.unimelb.edu.au/articles/dataset/Multi-Frequency_Steady-State_Visual_Evoked_Potential_Dataset/22015694))
 
 
 # Used Resources
